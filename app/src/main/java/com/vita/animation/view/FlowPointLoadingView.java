fcf1a023af0c06fedc47d5e8b3b936a4a50319c4
VitaAin <wtaincb@126.com>
2018-02-02 13:22:44 +0800

    finish FlowPointLoadingView

--- app/src/main/java/com/vita/animation/view/FlowPointLoadingView.java
+++ app/src/main/java/com/vita/animation/view/FlowPointLoadingView.java
@@ -10,8 +10,7 @@ import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
-import com.vita.animation.evaluator.ThirdOrderBezierEvaluator;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 /**
  * @FileName: com.vita.animation.view.FlowPointLoadingView.java
@@ -23,9 +22,16 @@ public class FlowPointLoadingView extends View {
 
     private static final String TAG = "FlowPointLoadingView";
     private Paint mRefLinePaint;
+    private Paint mCirclePaint;
     private int mWidth, mHeight;
     private float mCircleRadius = 20;
-    private Paint mCirclePaint;
+    private float mFlowPointRadius = mCircleRadius / 2;
+    private float mMixedCircleRadius = mCircleRadius + mFlowPointRadius;
+    private PointF mFlowPoint;
+    private ValueAnimator mAnim;
+    private int mCurPos;
+    private Path mPath;
+    private int mDirection = 1; // 1: left to right, -1: right to left
 
     public FlowPointLoadingView(Context context) {
         super(context);
@@ -69,6 +75,22 @@ public class FlowPointLoadingView extends View {
         drawFixedPoints(canvas);
         drawFlowPoint(canvas);
         drawPath(canvas);
+
+        updateCurPos();
+
+        if (mAnim == null || !mAnim.isRunning()) {
+            startAnim();
+        }
+    }
+
+    private void updateCurPos() {
+        for (int i = 0; i < 6; i++) {
+            if (Math.abs(mFlowPoint.x - mWidth / 7 * (i + 1)) < mWidth / 7 / 5 * 2) {
+                mCurPos = i + 1;
+                return;
+            }
+        }
+        mCurPos = 0;
     }
 
     private void drawRefLine(Canvas canvas) {
@@ -77,35 +99,52 @@ public class FlowPointLoadingView extends View {
 
     private void drawFixedPoints(Canvas canvas) {
         for (int i = 1; i < 7; i++) {
-            canvas.drawCircle(mWidth / 7 * i, mHeight / 2, mCircleRadius, mCirclePaint);
+            if (i == mCurPos) {
+                canvas.drawCircle(mWidth / 7 * i, mHeight / 2, mMixedCircleRadius, mCirclePaint);
+            } else {
+                canvas.drawCircle(mWidth / 7 * i, mHeight / 2, mCircleRadius, mCirclePaint);
+            }
         }
     }
 
     private void drawFlowPoint(Canvas canvas) {
-        canvas.drawCircle(mWidth / 7 + 60, mHeight / 2, mCircleRadius / 2, mCirclePaint);
+        if (mFlowPoint == null) {
+            mFlowPoint = new PointF(mWidth / 7, mHeight / 2);
+        }
+        canvas.drawCircle(mFlowPoint.x, mFlowPoint.y, mCircleRadius / 2, mCirclePaint);
     }
 
     private void drawPath(Canvas canvas) {
-        PointF ctrlPoint = new PointF(mWidth / 7 + 30, mHeight / 2);
+        if (mCurPos == 0) {
+            return;
+        }
 
-        Path path = new Path();
-        PointF[] fixedCutoffPoints0 = getFixedCutoffPoints(0);
-        path.moveTo(fixedCutoffPoints0[0].x, fixedCutoffPoints0[0].y);
-        path.quadTo(ctrlPoint.x, ctrlPoint.y, mWidth / 7 + 60, mHeight / 2 - mCircleRadius / 2);
-        path.lineTo(mWidth / 7 + 60, mHeight / 2 + mCircleRadius / 2);
-        path.quadTo(ctrlPoint.x, ctrlPoint.y, mWidth / 7, mHeight / 2 + mCircleRadius / 2);
-        path.lineTo(mWidth / 7, mHeight / 2 - mCircleRadius / 2);
-        path.close();
-        canvas.drawPath(path, mCirclePaint);
+        if (mPath == null) {
+            mPath = new Path();
+        }
+        mPath.reset();
+
+        PointF ctrlPoint = getCtrlPoint();
+        PointF[] fixedCutoffPoints0 = getFixedCutoffPoints();
+
+        mPath.moveTo(fixedCutoffPoints0[0].x, fixedCutoffPoints0[0].y);
+        mPath.quadTo(ctrlPoint.x, ctrlPoint.y, mFlowPoint.x, mFlowPoint.y - mFlowPointRadius);
+        mPath.lineTo(mFlowPoint.x, mFlowPoint.y + mFlowPointRadius);
+        mPath.quadTo(ctrlPoint.x, ctrlPoint.y, fixedCutoffPoints0[1].x, fixedCutoffPoints0[1].y);
+        mPath.lineTo(fixedCutoffPoints0[0].x, fixedCutoffPoints0[0].y);
+        mPath.close();
+        canvas.drawPath(mPath, mCirclePaint);
     }
 
     private PointF getCtrlPoint() {
-
-        return null;
+        PointF fixedCircleCenter = new PointF(mWidth / 7 * mCurPos, mHeight / 2);
+        float x = (fixedCircleCenter.x + mFlowPoint.x) / 2;
+        float y = mHeight / 2;
+        return new PointF(x, y);
     }
 
-    private PointF[] getFixedCutoffPoints(int position) { // position: 0-6
-        float x = mWidth / 7 * (position + 1);
+    private PointF[] getFixedCutoffPoints() {
+        float x = mWidth / 7 * (mCurPos);
         PointF[] pointFs = new PointF[2];
         pointFs[0] = new PointF(x, mHeight / 2 - mCircleRadius);
         pointFs[1] = new PointF(x, mHeight / 2 + mCircleRadius);
@@ -113,6 +152,21 @@ public class FlowPointLoadingView extends View {
     }
 
     private void startAnim() {
-//        ValueAnimator.ofObject(new ThirdOrderBezierEvaluator())
+        mAnim = ValueAnimator.ofFloat(mFlowPoint.x - mFlowPointRadius * 4,
+                mWidth / 7 * 6 + mFlowPointRadius * 4);
+        mAnim.setDuration(3000);
+        mAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+        mAnim.setRepeatCount(ValueAnimator.INFINITE);
+        mAnim.setRepeatMode(ValueAnimator.REVERSE);
+        mAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float value = (float) valueAnimator.getAnimatedValue();
+                mDirection = (value - mFlowPoint.x) > 0 ? 1 : -1;
+                mFlowPoint.x = value;
+                invalidate();
+            }
+        });
+        mAnim.start();
     }
 }
